61f2bf5e0646d3f76fefc72e9042c761102d4b65
Adam Ridhwan <adamridhwan.1@gmail.com>
2022-07-26 21:40:34 -0400

    adde debounce

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import './App.css';
+
+import debounce from 'lodash.debounce';
+
+const API_URL = 'https://jsonplaceholder.typicode.com/users';
 
 const App = () => {
   const [searchField, setSearchField] = useState('');
   const [users, setUsers] = useState([]);
   const [filteredUsers, setFilteredUser] = useState([]);
-  // console.log(filteredUsers);
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(data => setUsers(data));
-  }, []);
+    let ignore = false;
+    // console.log('get data');
+    (async () => {
+      if (!searchField) return;
+      let data;
+
+      try {
+        const response = await fetch(API_URL);
+        data = await response.json();
+      } catch (error) {
+        console.log(error);
+      }
+
+      if (!ignore) {
+        setUsers(data);
+      }
+    })();
+
+    return () => {
+      ignore = true;
+    };
+  }, [searchField]);
 
   useEffect(() => {
+    // console.log('display users');
     const newFilteredUsers = users.filter(user => {
       return user.name.toLocaleLowerCase().includes(searchField);
     });
-    setFilteredUser(newFilteredUsers);
+    searchField === ''
+      ? setFilteredUser([])
+      : setFilteredUser(newFilteredUsers);
   }, [users, searchField]);
 
   const onSearchChange = event => {
@@ -25,19 +49,34 @@ const App = () => {
     setSearchField(searchFieldString);
   };
 
+  const debouncedChangeHandler = useMemo(
+    () => debounce(onSearchChange, 300),
+    []
+  );
+
+  useEffect(() => {
+    return () => {
+      debouncedChangeHandler.cancel();
+    };
+  }, []);
+
   return (
-    <div className='content'>
-      <div>
+    <>
+      <h3>Small project using useState and useEffect hooks</h3>
+      <div className='input-box'>
         <input
-          onChange={onSearchChange}
-          placeholder='search for contact'
-        ></input>
+          type='text'
+          onChange={debouncedChangeHandler}
+          placeholder='Search for user'
+        />
       </div>
-      {filteredUsers.map(user => {
-        const { id, name } = user;
-        return <div key={id}>{name}</div>;
-      })}
-    </div>
+      <div className='content'>
+        {filteredUsers.map(user => {
+          const { id, name } = user;
+          return <div key={id}>{name}</div>;
+        })}
+      </div>
+    </>
   );
 };
 
